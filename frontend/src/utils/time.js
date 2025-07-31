7044883d5243323f67041ef0fc090056b31f93c3
Akshat Raj Saxena <119042958+akshatrajsaxena@users.noreply.github.com>
2025-07-31 20:05:08 +0530

    Create time.js

---
+++ frontend/src/utils/time.js
@@ -0,0 +1,7 @@
+export const getISTISOString = () => {
+    const now = new Date();
+    const istOffset = 5.5 * 60 * 60 * 1000;
+    const istTime = new Date(now.getTime() + istOffset);
+    return istTime.toISOString().replace('T', ' ').split('.')[0];
+  };
+  
